9a28656961f4b85373fb48492f7c461acd897df8
andrew-dave <147888175+andrew-dave@users.noreply.github.com>
2024-09-20 11:55:30 -0400

    Applied mask on original image to visualise segmentation

--- 7_HSV.py
+++ 7_HSV.py
@@ -1,4 +1,3 @@
-# Basic understanding of HSV colour segments 
 import cv2 as cv
 import os
 import numpy as np
@@ -11,22 +10,34 @@ def hsvColorseg():
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    lowerBound = np.array([0,0,50])
-    upperBound = np.array([150,140,180])
+    # Define the HSV range for color segmentation
+    lowerBound = np.array([0, 0, 50])
+    upperBound = np.array([150, 140, 180])
 
+    # Create mask
     mask = cv.inRange(imgHSV, lowerBound, upperBound)
 
+    # Apply the mask to the original image to visualize the segmentation
+    result = cv.bitwise_and(imgRGB, imgRGB, mask=mask)
+
+    # Display original image
     plt.figure()
     plt.imshow(imgRGB)
-    plt.title("RGB")
+    plt.title("RGB Image")
     plt.show()
-    # plt.subplot(232)
-    # plt.imshow(imgHSV)
-    # plt.title("HSV")
-    cv.imshow("mask", mask)
+
+    # Display the mask
+    cv.imshow("Mask", mask)
+
+    # Display the segmented image
+    plt.figure()
+    plt.imshow(result)
+    plt.title("Segmented Image")
+    plt.show()
+
+    # Close OpenCV windows
     cv.waitKey(0)
-
-
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     hsvColorseg()
